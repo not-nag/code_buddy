d216dba45f60d33bc6c8c279fd36ecf1e2853814
Nagraj Thamankar <nagrajtham@gmail.com>
2023-04-27 21:53:22 +0530

    fixed header

--- components/IndividualComponents/Header.js
+++ components/IndividualComponents/Header.js
@@ -1,16 +1,23 @@
 import styles from "../../styles/header.module.css";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <div className={styles.header}>
-      <Image
-        className={styles.logo}
-        src="/logo.png"
-        width={147}
-        height={80}
-      ></Image>
-      <div className={styles.people}></div>
+      <Link href="/">
+        <Image
+          className={styles.logo}
+          src="/logo.png"
+          width={147}
+          height={80}
+        ></Image>
+      </Link>
+      {router.pathname === "/[room_id]" ? (
+        <div className={styles.people}></div>
+      ) : null}
     </div>
   );
 }
