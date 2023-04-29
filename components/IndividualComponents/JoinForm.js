f9b8bd1fdd65ee84c2d527bcd8ba506854330667
Nagraj Thamankar <nagrajtham@gmail.com>
2023-04-29 20:40:12 +0530

    useState for input

--- components/IndividualComponents/JoinForm.js
+++ components/IndividualComponents/JoinForm.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import styles from "../../styles/join_form.module.css";
 
 export default function JoinForm() {
+  const [roomId, setRoomId] = useState("");
+  const [roomPassword, setRoomPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={styles.form_parent}>
       <h1 className={styles.h1}>Enter the details to join a room</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <p className={styles.p}>Enter the Room ID</p>
         <input
           type="text"
           name="room_id"
           className={styles.input}
           placeholder="Room ID"
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
           required
         />
         <p className={styles.p}>Enter the Room password</p>
@@ -19,6 +29,8 @@ export default function JoinForm() {
           name="room_password"
           className={styles.input}
           placeholder="Room Password"
+          value={roomPassword}
+          onChange={(e) => setRoomPassword(e.target.value)}
           required
         />
         <br />
