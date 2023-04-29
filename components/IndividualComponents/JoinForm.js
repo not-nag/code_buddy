1a6c5b731a8932a22d81b660415d8dc54f300626
Nagraj Thamankar <nagrajtham@gmail.com>
2023-04-29 06:45:03 +0530

    join form updates

--- components/IndividualComponents/JoinForm.js
+++ components/IndividualComponents/JoinForm.js
@@ -1,5 +1,14 @@
 import styles from "../../styles/join_form.module.css";
 
 export default function JoinForm() {
-  return <h1>I am form</h1>;
+  return (
+    <div className={styles.form_parent}>
+      <form>
+        <label htmlFor="room_id">Enter the Room ID</label>
+        <input type="text" name="room_id" />
+        <label htmlFor="room_password">Enter the room password</label>
+        <input type="text" name="room_password" />
+      </form>
+    </div>
+  );
 }
