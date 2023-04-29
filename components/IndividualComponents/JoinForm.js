7b7c4d2e9f78d3e226503c003685dca76c9faa41
Nagraj Thamankar <nagrajtham@gmail.com>
2023-04-29 07:02:16 +0530

    basic form design

--- components/IndividualComponents/JoinForm.js
+++ components/IndividualComponents/JoinForm.js
@@ -3,14 +3,14 @@ import styles from "../../styles/join_form.module.css";
 export default function JoinForm() {
   return (
     <div className={styles.form_parent}>
-      <h1>Enter the details to join a room</h1>
+      <h1 className={styles.h1}>Enter the details to join a room</h1>
       <form>
-        <label htmlFor="room_id">Enter the Room ID</label>
+        <p className={styles.p}>Enter the Room ID</p>
         <input type="text" name="room_id" />
-        <label htmlFor="room_password">Enter the room password</label>
+        <p className={styles.p}>Enter the room password</p>
         <input type="text" name="room_password" />
       </form>
-      <h1>OR</h1>
+      <h1 className={styles.h1}>OR</h1>
       <button>Click here to create a room</button>
     </div>
   );
