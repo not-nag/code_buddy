4cd92bffaa5c8c7a3d82baadb6b9dd38b2b796a7
Nagraj Thamankar <nagrajtham@gmail.com>
2023-04-30 17:44:23 +0530

    fixed back buttons

--- components/Dev.js
+++ components/Dev.js
@@ -1,8 +1,20 @@
 import Header from "./IndividualComponents/Header";
 import Code from "./IndividualComponents/Code";
-
+import { useEffect, useContext } from "react";
+import { UserContext } from "./Context/UserAuth";
 
 export default function Dev() {
+  const { setUser } = useContext(UserContext);
+  useEffect(() => {
+    window.onpopstate = () => {
+      setUser(false);
+    };
+
+    window.onbeforeunload = () => {
+      setUser(false);
+    };
+  });
+
   return (
     <>
       <Header></Header>
